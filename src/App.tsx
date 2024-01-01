3c36261382f3c5d8e52b6b40e90fb6650081ea24
Snoodelz <ludwig.mostrom@gmail.com>
2024-01-01 19:12:51 +0100

    changes pocketbase url

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("https://pb.lumopi.se/");
+const pb = new PocketBase("https://lumopi.se/pb");
 
 interface Grocery {
 	collectionId: string;
